40a096b747b1b9b1279ded2ba556fe00637d62af
Shaik Asif <asif118shaik@gmail.com>
2026-07-04 13:33:07 +0530

    A simple program for Quotient & Remainder

---
+++ Quo_Rem.py
@@ -0,0 +1,7 @@
+#quotient and remainder
+num=int(input("Enter a number: "))
+divisor=int(input("Enter the divisor: "))
+quotient=num/divisor
+remainder=num%divisor
+print(f"The quotient of {num} and {divisor} is {quotient:.2f}")
+print(f"The remainder of {num} and {divisor} is {remainder}")
