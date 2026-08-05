1961f7028b2f84e30d15cbd1f5a0c171e068662f
Shaik Asif <asif118shaik@gmail.com>
2026-08-05 23:25:57 +0530

    Finding the letter is upper or lowercase or spedial char

---
+++ FindingLetters.py
@@ -0,0 +1,9 @@
+ch=input("Enter a character: ")
+if  'A' <= ch <= 'Z':
+    print("Upper Case")
+elif 'a' <= ch <= 'z':
+    print("Lower case")
+elif '0' <= ch <='1':
+    print("digit")
+else:
+    print("Special Character")
